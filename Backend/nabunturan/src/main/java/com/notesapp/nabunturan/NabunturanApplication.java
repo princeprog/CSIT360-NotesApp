341792358879c279d192f585fded3f5307b002a4
alprince <132316008+princeprog@users.noreply.github.com>
2025-09-22 17:07:20 +0800

    Initialize Spring Boot project for Nabunturan
    Add initial Spring Boot application structure for Nabunturan, including Maven wrapper, project configuration files, main application class, and a basic test. This sets up the foundation for further backend development.

---
+++ Backend/nabunturan/src/main/java/com/notesapp/nabunturan/NabunturanApplication.java
@@ -0,0 +1,13 @@
+package com.notesapp.nabunturan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NabunturanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NabunturanApplication.class, args);
+	}
+
+}
---
+++ Backend/nabunturan/src/test/java/com/notesapp/nabunturan/NabunturanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notesapp.nabunturan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NabunturanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
